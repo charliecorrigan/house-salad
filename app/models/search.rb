50b4ec9421af37f8ba110243118d14c6581db203
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-12 11:11:40 -0600

    Remove commented code from search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,14 +4,6 @@ class Search
   end
 
   def search_by_state
-    # conn = Faraday.new(:url => "https://api.propublica.org/congress/v1/members/house/#{state}/current.json") 
-    # response = conn.get do |req|
-    #   req.url "https://api.propublica.org/congress/v1/members/house/#{state}/current.json"
-    #   req.headers['X-API-Key'] = ENV["propublica_key"]
-    # end
-
-    # unsorted_results = JSON.parse(response.body)["results"]
-    # @search_results = unsorted_results.sort_by { |member| member["seniority"].to_i }.reverse
     unsorted_search_results = PropublicaService.new(state).search_by_state
     search_results = unsorted_search_results.sort_by { |member| member["seniority"].to_i }.reverse
   end
