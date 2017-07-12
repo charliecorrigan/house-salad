63167ea98bf0045585940a4daa7d22ed78ad9505
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-12 10:18:46 -0600

    Move api key to application.yml

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     conn = Faraday.new(:url => 'https://api.propublica.org/congress/v1/members/house/CO/current.json') 
     response = conn.get do |req|
       req.url 'https://api.propublica.org/congress/v1/members/house/CO/current.json'
-      req.headers['X-API-Key'] = 'ENV['propublica_key']'
+      req.headers['X-API-Key'] = ENV["propublica_key"]
     end
 
     @search_results = JSON.parse(response.body)
