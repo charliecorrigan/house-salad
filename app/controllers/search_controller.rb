8885985017406de73c3e72dd74c0a950abc3f544
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-12 09:58:53 -0600

    Add api call to search controller, in progress

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get("https://api.propublica.org/congress/v1/members/house/CO/current.json")
+    @search_results = JSON.parse(response.body)
+    binding.pry
   end
 
 end
