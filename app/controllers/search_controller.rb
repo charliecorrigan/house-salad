3534a3905be247081607db224c1d8c3390baccad
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-12 10:16:56 -0600

    Set header info in api call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://api.propublica.org/congress/v1/members/house/CO/current.json")
+    conn = Faraday.new(:url => 'https://api.propublica.org/congress/v1/members/house/CO/current.json') 
+    response = conn.get do |req|
+      req.url 'https://api.propublica.org/congress/v1/members/house/CO/current.json'
+      req.headers['X-API-Key'] = 'ENV['propublica_key']'
+    end
+
     @search_results = JSON.parse(response.body)
-    binding.pry
   end
 
 end
